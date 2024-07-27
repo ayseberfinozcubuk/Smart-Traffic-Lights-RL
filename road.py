d0c8500aa3d1716abe1c37fea3a9fe7192db32cd
Ayşe Berfin Özçubuk <85034235+ayseberfinozcubuk@users.noreply.github.com>
2024-07-27 17:06:34 +0300

    finds num of cars in the stopping area

--- road.py
+++ road.py
@@ -1,6 +1,9 @@
-# road.py
-
 import pygame
+from enum import Enum
+
+class Direction(Enum):
+    HORIZONTAL = 1
+    VERTICAL = 2
 
 class Road:
     def __init__(self, start_x, start_y, end_x, end_y, color) -> None:
@@ -9,13 +12,41 @@ class Road:
         self.end_x = end_x
         self.end_y = end_y
         self.color = color
+        self.direction = Direction.VERTICAL
+        if abs(self.start_x - self.end_x) > abs(self.start_y - self.end_y):
+            self.direction = Direction.HORIZONTAL
+        self.stopping_area = self.calculate_stopping_area()
+
+    def calculate_stopping_area(self):
+        if self.direction == Direction.HORIZONTAL:
+            stop_end_x = self.start_x + (self.end_x - self.start_x) // 2
+            return (self.start_x, self.start_y, stop_end_x, self.end_y)
+        else:
+            stop_end_y = self.start_y + (self.end_y - self.start_y) // 2
+            return (self.start_x, self.start_y, self.end_x, stop_end_y)
 
     def draw(self, surface):
         width = abs(self.end_x - self.start_x)
         height = abs(self.end_y - self.start_y)
-        
         pygame.draw.rect(surface, self.color, pygame.Rect(self.start_x, self.start_y, width, height))
 
+    def draw_stopping_area(self, surface, color):
+        width = abs(self.stopping_area[2] - self.stopping_area[0])
+        height = abs(self.stopping_area[3] - self.stopping_area[1])
+        pygame.draw.rect(surface, color, pygame.Rect(self.stopping_area[0], self.stopping_area[1], width, height))
+
     def is_inside(self, x, y):
-        # Check if a point (x, y) is within the rectangle representing the road
         return (self.start_x <= x <= self.end_x) and (self.start_y <= y <= self.end_y)
+    
+    def count_cars_in_area(self, all_cars):
+        count = 0
+        for car in all_cars:
+            car_center_x = car.x + car.width / 2
+            car_center_y = car.y + car.height / 2
+            
+            if (self.stopping_area[0] <= car_center_x <= self.stopping_area[2]) and (self.stopping_area[1] <= car_center_y <= self.stopping_area[3]):
+                count += 1
+        return count
+
+    def get_size(self):
+        return abs(self.end_x - self.start_x), abs(self.end_y - self.start_y)
--- two_road.py
+++ two_road.py
@@ -13,44 +13,46 @@ gray = (60, 60, 60)
 light_gray = (211, 211, 211)
 red = (255, 0, 0)
 green = (100, 200, 70)
+abc = (150, 80, 50)
 
 road_width = 50
 
-vertical_road = Road(0, height // 2 - road_width, width, height // 2 + road_width, gray)
-vertical_road_small = Road(width // 2 - 100, height // 2 - road_width, width // 2 - road_width, height // 2 + road_width, green)
-horizontal_road = Road(width // 2 - road_width, 0, width // 2 + road_width, height, gray)
+horizontal_road = Road(0, height // 2 - road_width, width, height // 2 + road_width, gray)
+horizontal_road_small = Road(width // 2 - 100, height // 2 - road_width, width // 2 - road_width, height // 2 + road_width, green)
+vertical_road = Road(width // 2 - road_width, 0, width // 2 + road_width, height, gray)
+'''
 top_left_grass = Road(0, 0, width // 2 - road_width, height // 2 - road_width, green)
 top_right_grass = Road(width // 2 + road_width, 0, width, height // 2 - road_width, green)
 bottom_left_grass = Road(0, height // 2 + road_width, width // 2 - road_width, height, green)
 bottom_right_grass = Road(width // 2 + road_width, height // 2 + road_width, width, height, green)
+'''
 
 car_spawner1 = CarSpawner((0, 0, height // 2), red, (50, 30), 2000, (1, 0), speed_reduction_distance=100)
 car_spawner2 = CarSpawner((width // 2, width // 2, 0), red, (50, 30), 2000, (0, 1), speed_reduction_distance=100)
 
-traffic_light = TrafficLight(width // 2 - 50, height // 2, vertical_road_small)
+traffic_light = TrafficLight(width // 2 - 50, height // 2, horizontal_road_small)
 
 traffic_lights = [traffic_light]
 all_cars = []
 
-# Function to draw the roads and cars
 def draw_scene():
     screen.fill(light_gray)
-    vertical_road.draw(screen)
     horizontal_road.draw(screen)
-    vertical_road_small.draw(screen)
+    vertical_road.draw(screen)
+    horizontal_road_small.draw(screen)
+    '''
     top_left_grass.draw(screen)
     top_right_grass.draw(screen)
     bottom_left_grass.draw(screen)
     bottom_right_grass.draw(screen)
+    '''
     traffic_light.draw(screen)
     for car in all_cars:
         car.draw(screen)
 
-# Main game loop
 clock = pygame.time.Clock()
 start_time = pygame.time.get_ticks()
 
-# Time tracking for light change
 light_change_interval = 5000  # 5000 ms = 5 seconds
 last_light_change_time = pygame.time.get_ticks()
 
@@ -62,7 +64,6 @@ while True:
             pygame.quit()
             sys.exit()
     
-    # Update car spawners
     car_spawner1.spawn_car(current_time, all_cars)
     car_spawner2.spawn_car(current_time, all_cars)
 
@@ -70,7 +71,6 @@ while True:
         car.update(all_cars, traffic_lights, car_spawner1.speed_reduction_distance)
     
     if current_time - last_light_change_time >= light_change_interval:
-        # Change the light state
         next_light = {
             Light.RED: Light.GREEN,
             Light.GREEN: Light.RED,
@@ -78,8 +78,13 @@ while True:
         traffic_light.change_light(next_light)
         last_light_change_time = current_time
     
+    num_cars_in_hor_area = horizontal_road.count_cars_in_area(all_cars)
+    num_cars_in_ver_area = vertical_road.count_cars_in_area(all_cars)
+    print(f"Number of cars in horizontal area: {num_cars_in_hor_area}")
+    print(f"Number of cars in vertical area: {num_cars_in_ver_area}")
+
     draw_scene()
-    
+
     pygame.display.flip()
     
     clock.tick(60)
